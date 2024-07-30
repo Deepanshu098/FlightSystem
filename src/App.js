e49c8aacd94db5fa01d44acc70dac9cceb10b332
Deepanshu098 <deepanshusharma882@gmail.com>
2024-07-30 21:22:28 +0530

    add vapid key to .env file

--- src/App.js
+++ src/App.js
@@ -14,13 +14,13 @@ function App() {
       // Generate Token
       const token = await getToken(messaging, {
         vapidKey:
-          "BHmQKEocB7eIsStAJsAkxsqXFMfm5nLfMaSqwDgm0I1dLe94QZnFzU8xMnyjnAsvjvtuHKw3xf6-BzrQb5-QLRA",
+          process.env.VAPID_KEY,
       });
       console.log("Token Gen", token);
       // Send this token  to server ( db)
 
       Cookies.set('token',token,{ secure: true, sameSite: 'strict' })
-      //  await axios.post('http://localhost:8080/api/flight/fcmtoken',{token:token})
+       await axios.post('http://localhost:8080/api/flight/fcmtoken',{token:token})
       
     } else if (permission === "denied") {
       alert("You denied for the notification");
